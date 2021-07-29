e6cd9da04d96cbbb50a1453adf3b63ac27034124
lhw2106 <lhw2106@naver.com>
2021-07-29 09:48:48 +0900

    debug false

--- config/settings.py
+++ config/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*+k&nt)x8itt%3ph@^rom5ldera^xnwqnku!v!s*rk^#5bqtx)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 ALLOWED_HOSTS = ['*']
